52d097e93dba67ce578cc3af830b4aa8158f98a7
richard <zrichardzrd@gmail.com>
2026-09-18 16:46:09 -0300

    Refactor code to use funcions and add power/modulo operation

--- calc.py
+++ calc.py
@@ -1,23 +1,29 @@
+def calculate(num1, num2, operacao):
+    if operacao == '+':
+        return num1 + num2
+    elif operacao == '-':
+        return num1 - num2
+    elif operacao == '*':
+        return num1 * num2
+    elif operacao == '/':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Erro: Não é possível dividir por zero!"
+    elif operacao == "**":
+        return num1 ** num2
+    elif operacao == '%':
+        return num1 % num2
+    else:
+        return "Operação inválida"
 while True:
     # Pede os número para usuário 
     num1 = float(input("Digite o primeiro número: "))
-    operacao = input("Digite a operação (+, -, *, /): ")
+    operacao = input("Digite a operação (+, -, *, /, **, %): ")
     num2 = float(input("Digite o segundo número"))
 
-    #   Verificar qual foi a operação escolhida e faz o cálculo
-    if  operacao == '+':
-        resultado = num1 + num2
-    elif operacao ==  '-':
-        resultado = num1 - num2
-    elif operacao == '*':
-        resultado = num1 * num2
-    elif operacao == '/':
-            if num2 !=  0:
-                resultado = num1 / num2
-            else:
-                resultado = "Erro:  Não é possível dividir por zero!"
-    else: 
-        resultado = "Operação invalida"
+    # Chama a função de cálculo
+    resultado = calculate(num1, num2, operacao)
 
     #   Mostra o resultado na tela 
     print(f"O resultado é: {resultado}")
