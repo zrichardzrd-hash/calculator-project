5fc816b31bc8cf239e976b5df25b57c0a89f6d7e
richard <zrichardzrd@gmail.com>
2026-09-18 16:28:11 -0300

    Add loop and exit option to calculator

--- calc.py
+++ calc.py
@@ -1,22 +1,29 @@
-# Pede os número para usuário 
-num1 = float(input("Digite o primeiro número: "))
-operacao = input("Digite a operação (+, -, *, /): ")
-num2 = float(input("Digite o segundo número"))
+while True:
+    # Pede os número para usuário 
+    num1 = float(input("Digite o primeiro número: "))
+    operacao = input("Digite a operação (+, -, *, /): ")
+    num2 = float(input("Digite o segundo número"))
 
-#   Verificar qual foi a operação escolhida e faz o cálculo
-if  operacao == '+':
-    resultado = num1 + num2
-elif operacao ==  '-':
-    resultado = num1 - num2
-elif operacao == '*':
-    resultado = num1 * num2
-elif operacao == '/':
-        if num2 !=  0:
-            resultado = num1 / num2
-        else:
-            resultado = "Erro:  Não é possível dividir por zero!"
-else: 
-    resultado = "Operação invalida"
+    #   Verificar qual foi a operação escolhida e faz o cálculo
+    if  operacao == '+':
+        resultado = num1 + num2
+    elif operacao ==  '-':
+        resultado = num1 - num2
+    elif operacao == '*':
+        resultado = num1 * num2
+    elif operacao == '/':
+            if num2 !=  0:
+                resultado = num1 / num2
+            else:
+                resultado = "Erro:  Não é possível dividir por zero!"
+    else: 
+        resultado = "Operação invalida"
 
-#   Mostra o resultado na tela 
-print(f"O resultado é: {resultado}")
+    #   Mostra o resultado na tela 
+    print(f"O resultado é: {resultado}")
+    # Pergunta se o usuário quer continuar
+    continuar = input("Deseja fazer outra opeação? (s/n): ")
+
+    if continuar.lower() != 's':
+        print("Encerrando a calculadora; Até logo!")
+        break
